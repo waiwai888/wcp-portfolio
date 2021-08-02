5049c2e3edf3e1ea447c13a3c7b3241009988255
waiwai888 <yuisato9688@gmail.com>
2021-08-02 12:04:39 +0000

    ルーティング作成、ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'homes#top'
+  get '/search', to: 'searches#search'
+  get 'relationships/create'
+  get 'relationships/destroy'
+
+  resources :users, only: [:edit, :update]do
+    resources :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+  	get 'followers' => 'relationships#followers', as: 'followers'
+  end
+  
+  resources :posts, only: [:new, :create, :index, :show, :edit, :update]do
+    resources :post_comments, only: [:create, :destroy]
+    resources :notifications, only: [:create, :destroy, :all_destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
+  
+  resources :chats, only: [:show, :create]
+
 end
