cdb03643170ab387392edeffb3720c9c71449c4d
waiwai888 <yuisato9688@gmail.com>
2021-08-04 07:39:04 +0000

    いいね機能作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   attachment :profile_image
-         
-  has_many :favoritespost_images, dependent: :destroy
+
+  has_many :favorites, dependent: :destroy
   has_many :posts, dependent: :destroy
   has_many :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
   has_many :post_comments, dependent: :destroy
-  
+
   #通知機能
   has_many :active_notifications, class_name: "Notification", foreign_key: "visiter_id", dependent: :destroy
   has_many :passive_notifications, class_name: "Notification", foreign_key: "visited_id", dependent: :destroy
-  
+
   #フォロー機能
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :followings, through: :relationships, source: :followed
-  
+
   validates :name, presence: true, uniqueness: true
   validates :account_name, presence: true, uniqueness: true
   validates :nickname, presence: true
@@ -48,5 +48,5 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
-  
+
 end
