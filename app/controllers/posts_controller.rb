62d918912e469cb3d6d208cc4a0c7a455853f78b
waiwai888 <yuisato9688@gmail.com>
2021-08-05 04:04:31 +0000

    follow機能作成（ビュー再調整の必要あり）、DM機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @user = @post.user
     @post_comment = PostComment.new
     @post_comments = PostComment.all
   end
