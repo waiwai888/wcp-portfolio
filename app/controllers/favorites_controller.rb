ac1ad70100345f31e1255406fcf644fa8cfa5692
waiwai888 <yuisato9688@gmail.com>
2021-08-06 14:37:38 +0000

    notice機能作成（一覧表示調整中）、いいねぼたん調整必要あり

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,6 +4,7 @@ class FavoritesController < ApplicationController
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.new(post_id: post.id)
     favorite.save
+    post.create_notification_favorite!(current_user)
     redirect_back(fallback_location: root_path)
   end
 
--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,11 +1,19 @@
 class NotificationsController < ApplicationController
-  
+
   def index
+    @notifications = Notification.where(visited_id: current_user.id, checked: false)
+      # 通知の確認済みをtrueに変更して保存したいが、indexだとひとつの通知をどうピックアップすれば良いか。
+      # ひとつ取り出せれば、それをupdate_attributesで確認済みに変更
+    @notifications.each do |notification|
+      notification.update_attributes(checked: true)
+      @comment = PostComment.find_by(id: notification.comment)
+      @time = time_ago_in_words(notification.created_at).upcase
+    end
   end
 
   def destroy
   end
-  
+
   def all_destroy
   end
 
