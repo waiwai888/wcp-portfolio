ba5a1233d47158622592ab8e0e3eaeedd4b1135a
waiwai888 <yuisato9688@gmail.com>
2021-08-15 10:09:41 +0000

    users show/follow-indexレイアウト調整

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -15,12 +15,12 @@ class RelationshipsController < ApplicationController
 
   def followings
     @user = User.find(params[:user_id])
-    @users = @user.followings
+    @users = @user.followings.page(params[:page]).per(10)
   end
 
   def followers
     @user = User.find(params[:user_id])
-    @users = @user.followers
+    @users = @user.followers.page(params[:page]).per(10)
   end
 
 end
