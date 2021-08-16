45daeb99e26d07d46e173e83dcac7ee0fd6083d5
waiwai888 <yuisato9688@gmail.com>
2021-08-16 13:19:46 +0000

    検索窓でのタグ・投稿検索機能作成

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,5 +4,12 @@ class Tag < ApplicationRecord
   has_many :posts, through: :post_tags
   # post_tags（中間テーブル）との関連づけ。foreign_keyにtag_id（tag.idを入れて）を指定し、postを抽出
 
+  # 検索窓でのタグ検索
+  def self.search(content)
+    tag = Tag.where(tag_name: content).first
+    postIds = PostTag.where(tag_id: tag.id).pluck("post_id")
+    posts = Post.where(id: [postIds]) #postIdsの数だけpostを抽出
+    return posts
+  end
 
 end
