26bb6e0a64ee072a47589308a2ef76c02599b3c5
waiwai888 <yuisato9688@gmail.com>
2021-08-20 05:23:37 +0000

    通知一覧レイアウト修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,15 +2,16 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    user = User.find(params[:user_id])
+    @user = User.find(params[:user_id])
+    @post = Post.where(user_id: @user)
     current_user.follow(params[:user_id])
-    user.create_notification_follow!(current_user)
-    redirect_back(fallback_location: root_path)
+    @user.create_notification_follow!(current_user)
   end
 
   def destroy
+    @user = User.find(params[:user_id])
+    @post = Post.where(user_id: @user)
     current_user.unfollow(params[:user_id])
-    redirect_back(fallback_location: root_path)
   end
 
   def followings
