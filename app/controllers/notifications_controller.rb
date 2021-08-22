548c6ad1751f2976b79f544d83acec3878f284d4
waiwai888 <yuisato9688@gmail.com>
2021-08-22 12:54:31 +0000

    mainにpush

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,8 +2,7 @@ class NotificationsController < ApplicationController
 
   def index
     @notifications = Notification.where(visited_id: current_user.id, checked: false).where.not(visiter_id: current_user.id).to_a
-      # 通知の確認済みをtrueに変更して保存したいが、indexだとひとつの通知をどうピックアップすれば良いか。
-      # ひとつ取り出せれば、それをupdate_attributesで確認済みに変更
+      # 通知の確認済みをtrueに変更して保存したいため、.to_aで配列とし、その後checked: trueにすることで表示を可能に。
     @notifications.each do |notification|
       notification.update_attributes(checked: true)
       @comment = PostComment.find_by(id: notification.comment_id)
