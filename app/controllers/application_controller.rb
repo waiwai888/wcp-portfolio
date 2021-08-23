aa729cb6c04362635aca5d4190de959ecc74df2e
waiwai888 <yuisato9688@gmail.com>
2021-08-23 07:15:28 +0000

    users/edit編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   
+  
   def after_sign_in_path_for(resource) 
-    posts_path
+    posts_followed_index_path
   end
 
   protected
