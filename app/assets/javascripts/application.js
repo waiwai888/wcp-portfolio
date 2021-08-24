454b1553d7951d39fa7756079860b8970dddcaf9
waiwai888 <yuisato9688@gmail.com>
2021-08-24 05:16:21 +0000

    jsbxsliderコメントアウト

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,28 +33,28 @@ $(function () {
     });
 });
 //ページ上部（top）へスクロール
-// $(function() {
-//   $('#back a').on('click',function(event){
-//     $('body, html').animate({
-//       scrollTop:0
-//     }, 800);
-//     event.preventDefault();
-//   });
-// });
-//top画像スクロール
-$(document).on('turbolinks:load', function(){
-  $('.bxslider').bxSlider({
-    auto: true,           // 自動スライド
-    speed: 1000,          // スライドするスピード
-    moveSlides: 1,        // 移動するスライド数
-    pause: 3000,          // 自動スライドの待ち時間
-    maxSlides: 1,         // 一度に表示させる最大数
-    randomStart: true,    // 最初に表示するスライドをランダムに設定
-    responsive: true,     //レスポンシブ対応
-    wrapperClass: 'bx-wrapper',//bx-weapperにクラス付与
-    autoHover: true       // ホバー時に自動スライドを停止
+$(function() {
+  $('#back a').on('click',function(event){
+    $('body, html').animate({
+      scrollTop:0
+    }, 800);
+    event.preventDefault();
   });
 });
+//top画像スクロール
+// $(document).on('turbolinks:load', function(){
+//   $('.bxslider').bxSlider({
+//     auto: true,           // 自動スライド
+//     speed: 1000,          // スライドするスピード
+//     moveSlides: 1,        // 移動するスライド数
+//     pause: 3000,          // 自動スライドの待ち時間
+//     maxSlides: 1,         // 一度に表示させる最大数
+//     randomStart: true,    // 最初に表示するスライドをランダムに設定
+//     responsive: true,     //レスポンシブ対応
+//     wrapperClass: 'bx-wrapper',//bx-weapperにクラス付与
+//     autoHover: true       // ホバー時に自動スライドを停止
+//   });
+// });
 // 通知タブ機能
 document.addEventListener("turbolinks:load", function() {
   $('#tab-contents .tab[id != "tab1"]').hide();
