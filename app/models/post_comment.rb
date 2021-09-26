71dc2b93bb9c7cbd9744c87b033e99deadb0dd8a
waiwai888 <yuisato9688@gmail.com>
2021-09-26 14:36:32 +0000

    コメントアウト（不要）削除

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -3,7 +3,6 @@ class PostComment < ApplicationRecord
   belongs_to :user
   belongs_to :post
   has_many :post_tags
-  # has_many :notifications, dependent: :destroy
 
   validates :comment, presence: true
 
