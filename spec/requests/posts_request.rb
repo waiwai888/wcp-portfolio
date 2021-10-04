5750adaa22b030c33f1608ecf4e431b5b9750ed3
waiwai888 <yuisato9688@gmail.com>
2021-10-04 13:38:15 +0000

    [RSpec]posts/アクセス制限追加

--- spec/requests/posts_request.rb
+++ spec/requests/posts_request.rb
@@ -14,4 +14,54 @@ RSpec.describe "Posts", type: :request do
       end
     end
   end
+  describe "index" do
+    #未ログインユーザーの場合
+    context "as a user not to login" do
+      # indexアクション後ログインページへ遷移
+      it "redirects to the sign-in page" do
+        get '/posts'
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+  describe "show" do
+    #未ログインユーザーの場合
+    context "as a user not to login" do
+      # showアクション後ログインページへ遷移
+      it "redirects to the sign-in page" do
+        get '/posts/:id'
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+  describe "show" do
+    #未ログインユーザーの場合
+    context "as a user not to login" do
+      # showアクション後ログインページへ遷移
+      it "redirects to the sign-in page" do
+        get '/posts/:id'
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+  describe "followed_index" do
+    #未ログインユーザーの場合
+    context "as a user not to login" do
+      # showアクション後ログインページへ遷移
+      it "redirects to the sign-in page" do
+        get '/posts/followed_index'
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+  describe "edit" do
+    #未ログインユーザーの場合
+    context "as a user not to login" do
+      # showアクション後ログインページへ遷移
+      it "redirects to the sign-in page" do
+        get '/posts/:id/edit'
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
 end
